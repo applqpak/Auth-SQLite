07cccb7c422b5bf3a1094d042e1fa44c7a7d0a7b
applqpak <applqpak@gmail.com>
2017-01-09 13:19:44 +1100

    Create Model.php

---
+++ src/Auth/utils/Model.php
@@ -0,0 +1,51 @@
+<?php
+
+  class Model
+  {
+      private $db;
+
+      public function __construct()
+      {
+          $this->db = new PDO('sqlite:users');
+          $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+
+      public function register($u, $p)
+      {
+          $this->db->prepare('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR NOT NULL, password VARCHAR NOT NULL);')->execute();
+          $this->db->prepare('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR NOT NULL, password VARCHAR NOT NULL);')->execute();
+          $stmt    = $this->db->prepare('SELECT password FROM users WHERE username=:username;');
+          $stmt->bindValue(':username', $u);
+          $stmt->execute();
+          $results = $stmt->fetchAll();
+          if(count($results) >= 1)
+          {
+              return false;
+          }
+          $stmt = $this->db->prepare('INSERT INTO users(username, password) VALUES(:username, :password);');
+          $stmt->bindValue(':username', $u);
+          $stmt->bindValue(':password', $p);
+          $stmt->execute();
+          return true;
+      }
+
+      public function login($u, $p)
+      {
+          $this->db->prepare('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR NOT NULL, password VARCHAR NOT NULL);')->execute();
+          $stmt    = $this->db->prepare('SELECT password FROM users WHERE username=:username;');
+          $stmt->bindValue(':username', $u);
+          $stmt->execute();
+          $results = $stmt->fetchAll();
+          if(count($results) >= 1)
+          {
+              foreach($results as $row)
+              {
+                  if(($row['password'] === $p))
+                  {
+                      return true;
+                  }
+              }
+          }
+          return false;
+      }
+  }
